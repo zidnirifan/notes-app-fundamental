a62f99fda7028b0551322af121e54f3becfc8cb2
Zidni Rif'an <Zidni Rif'an>
2021-07-02 08:03:16 +0700

    authorization implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -16,7 +16,14 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', tags, body } = request.payload;
-      const noteId = await this._service.addNote({ title, tags, body });
+
+      const { id: credentialId } = request.auth.credentials;
+      const noteId = await this._service.addNote({
+        title,
+        body,
+        tags,
+        owner: credentialId,
+      });
 
       const response = h.response({
         status: 'success',
@@ -45,8 +52,9 @@ class NotesHandler {
     }
   }
 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes();
+  async getNotesHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+    const notes = await this._service.getNotes(credentialId);
     return {
       status: 'success',
       data: {
@@ -58,6 +66,9 @@ class NotesHandler {
   async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
+
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
 
       const note = await this._service.getNoteById(id);
 
@@ -92,6 +103,9 @@ class NotesHandler {
       this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
 
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
+
       await this._service.editNoteById(id, request.payload);
 
       return {
@@ -121,6 +135,9 @@ class NotesHandler {
   async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
+
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
 
       await this._service.deleteNoteById(id);
       return {
